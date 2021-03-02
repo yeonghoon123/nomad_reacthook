a43c94c6ee41b1d9a13dddcb39e9e1d0312dc9f2
kyh <haa3163314@naver.com>
2021-03-02 10:02:55 +0900

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
 
+ const useInput = (initialValue, validator) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = event =>{ 
+      const {
+        target: {value}
+      } = event;
+      let willUpdate = true;
+      if(typeof validator === "function"){
+          willUpdate = validator(value)
+        }
+      if(willUpdate) {
+        setValue(value);     
+      }
+    }
+    return { value, onChange }
+   }
+
+   
 function App() {
+  const name = useInput("Mr.")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello </h1>
+      <input placeholder="Name" {...name}/>
     </div>
   );
 }
